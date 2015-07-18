41c9635af6ba095cc5bf177ed6b1aba643683ab8
alidplus <alidoubleplus@gmail.com>
2015-07-19 00:51:13 +0430

    salam
    in chie inja ?

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>The HTML5 Herald</title>
+  <link rel="stylesheet" href="css/styles.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery.flot.js"></script>
+  <script src="js/custom.js"></script>
+</head>
+
+<body>
+	<div id="target"></div>
+	<div id="target2"></div>
+	<div id="target3">sww</div>
+</body>
+</html>
