32e43608fead00eaceb91b39eaebeaa9d20717d2
alidplus <alidoubleplus@gmail.com>
2015-07-19 00:52:39 +0430

    test summ
    hellowq this i sthe testin gingit hub sydystem

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,23 @@
 $(function() {
+
+		var d1 = [];
+		for (var i = 0; i < 14; i += 0.5) {
+			d1.push([i, Math.sin(i)]);
+		}
+
+		var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+
+		var d3 = [];
+		for (var i = 0; i < 14; i += 0.5) {
+			d3.push([i, Math.cos(i)]);
+		}
+
+		var d4 = [];
+		for (var i = 0; i < 14; i += 0.1) {
+			d4.push([i, Math.sqrt(i * 10)]);
+		}
+
+		var d5 = [];
+		for (var i = 0; i < 14; i += 0.5) {
+			d5.push([i, Math.sqrt(i)]);
+		}
